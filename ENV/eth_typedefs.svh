45e162415249e10b781d920a6e3f129aa1b8d594
Srinivasan Venkataramanan <srini@verifworks.com>
2021-02-21 21:59:44 +0530

    Added Makefile
    Signed-off-by: Srinivasan Venkataramanan <srini@verifworks.com>

---
+++ ENV/eth_typedefs.svh
@@ -0,0 +1,6 @@
+typedef class eth_packet_chk_c ;
+typedef class eth_packet_drv_c;
+typedef class eth_packet_gen_c;
+typedef class eth_packet_mon_c;
+typedef class eth_packet_c;
+typedef class packet_tb_env_c;
---
+++ run_dir/tb.f
@@ -0,0 +1,10 @@
+../ENV/eth_typedefs.svh
+../ENV/eth_sw_if.svh
+../ENV/eth_packet.svh
+../ENV/eth_packet_chk.svh
+../ENV/eth_packet_gen.svh
+../ENV/eth_packet_drv.svh
+../ENV/eth_packet_mon.svh
+../ENV/packet_tb_env.svh
+../ENV/packet_tb_top.sv
+
